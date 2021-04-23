66b0c01fdde4c03540cb37f932ddc0f94f0f3759
264154 <suprabhabhat754@gmail.com>
2021-04-23 11:26:57 +0530

    22/04/21

---
+++ python_20/2ndsmallest_list1.py
@@ -0,0 +1,7 @@
+
+def find_len(list1):
+    length = len(list1)
+    list1.sort()
+    print("Second Smallest element is:", list1[1])
+list1 = [121, 405, 2, 48, 31, 10, 18, 16, 44]
+Largest = find_len(list1)
