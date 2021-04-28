87571fbf0177c54fb2ccd5de8d61afe31c3177c0
264154 <suprabhabhat754@gmail.com>
2021-04-28 16:57:56 +0530

    27/04/21

---
+++ python_20/datatypes.py
@@ -0,0 +1,18 @@
+#string
+a = "This is a string"
+print (a)
+
+#list
+L = [1, "a" , "string" , 1+2]
+print(L)
+L.append(6)
+print(L)
+L.pop()
+print(L)
+print(L[1])
+
+#tuple
+tup = (1, "a", "string", 1+2)
+print(tup)
+print(tup[1])
+
